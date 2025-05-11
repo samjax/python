7c4e609ffdef1ee15178aca90eb7cb69178b2bfa
sampath <s@p.com>
2025-05-11 17:21:08 +0530

    did a practice of class method, class variable, using class method as alternate constructor

--- OOP_python/employee.py
+++ OOP_python/employee.py
@@ -1,16 +1,20 @@
 class Employee:
     raise_amount = 1.04
+    num_of_employees = 0
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = self.first + '.' + self.last + '@company.com'
-    
+
+        Employee.num_of_employees += 1
+
     def getFullName(self):
         return self.first.capitalize() + " " + self.last.capitalize()
 
     def apply_raise(self):
-        self.pay = int(self.pay * Employee.raise_amount)
+        self.pay = int(self.pay * self.raise_amount)
 
 emp_1 = Employee('sampath', 'kumar', 1000)
 emp_2 = Employee('corey', 'shafer', 2000)
@@ -24,9 +28,18 @@ emp_2 = Employee('corey', 'shafer', 2000)
 # print(emp_2.getFullName())
 #Callign using class name
 # print(Employee.getFullName(emp_1))
-print(emp_1.pay)
-emp_1.apply_raise()
-print(emp_1.pay)
+# emp_1.raise_amount = 1.10
+# emp_2.raise_amount = 1.20
+# print(emp_1.pay)
+# emp_1.apply_raise()
+# print(emp_1.pay)
 
-print(emp_1.__dict__)
-print(Employee.__dict__)
+# print(emp_2.pay)
+# emp_2.apply_raise()
+# print(emp_2.pay)
+
+# print(emp_1.__dict__)
+# print(Employee.__dict__)
+# print(emp_2.__dict__)
+
+print(f"Total num of employees: {Employee.num_of_employees}")
---
+++ OOP_python/practice_emp.py
@@ -0,0 +1,44 @@
+class Employee:
+    num_of_employees = 0
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = self.first + "." + self.last + "@company.com"
+
+        Employee.num_of_employees += 1
+
+    def getFullName(self):
+        return f"{self.first.capitalize() + ' ' + self.last.capitalize()}"
+    
+    @classmethod
+    def update_raise_amt(cls, new_raise_amount):
+        cls.raise_amount = new_raise_amount
+
+    #using class method let's create an alternate constructor
+    @classmethod
+    def from_empstring(cls, empl_str):
+        first, last, pay = empl_str.split('-')
+        return cls(first, last, pay)
+
+emp_1 = Employee('sampath','kumar',110000)
+emp_2 = Employee('Amit','Sharma',110000)
+
+print(emp_1.getFullName())
+print(emp_2.getFullName())
+print(f"Total number of emps: {Employee.num_of_employees}")
+
+#get raise amount...again
+print(f"Current raise_amount: {emp_1.raise_amount}")
+#update employee raise amount
+Employee.update_raise_amt(1.25)
+#get raise amount...again
+print(f"Current raise_amount: {emp_1.raise_amount}")
+
+#create a new employee object using the alternate constructor
+#you pass employee details using a string like this sampath-periasamy-70000
+new_employee = Employee.from_empstring('Elon-Musk-10')
+print(new_employee.getFullName())
+print(f"Total number of emps: {Employee.num_of_employees}")
